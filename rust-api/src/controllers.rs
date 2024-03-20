7700b3b83df9bb9870c06ce007a2914333247955
Psykka <36780964+Psykka@users.noreply.github.com>
2024-03-20 07:29:37 +0000

    fix: add text return to healt

--- rust-api/src/controllers.rs
+++ rust-api/src/controllers.rs
@@ -4,7 +4,7 @@ use log::info;
 pub mod auth_controller;
 pub mod user_controller;
 
-pub async fn health() -> http::StatusCode {
+pub async fn health() -> (http::StatusCode, &'static str) {
     info!("Health checked");
-    http::StatusCode::OK
+    (http::StatusCode::OK, "OK")
 }
